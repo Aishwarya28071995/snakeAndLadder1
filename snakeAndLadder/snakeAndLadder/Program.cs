05dae1346de1a1fb84176cb1755c2bcc599be777
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-12 00:42:32 +0530

    Commting initial files

---
+++ snakeAndLadder/snakeAndLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace snakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+        }
+    }
+}
