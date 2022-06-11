6e19c98402fc7e1ee68aed42e88260e4749c9541
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-12 00:49:51 +0530

    Commiting UC1 files

--- snakeAndLadder/snakeAndLadder/Program.cs
+++ snakeAndLadder/snakeAndLadder/Program.cs
@@ -9,6 +9,10 @@ namespace snakeAndLadder
             Console.WriteLine("Snake and Ladder game played with single player at start position 0");
             int position = 0;
             Console.WriteLine("Position is "+position);
+
+            Random random = new Random();
+            int number = random.Next(0, 7);
+            Console.WriteLine(number);
         }
     }
 }
